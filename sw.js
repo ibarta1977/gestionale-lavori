3dc0e1c77baa7cc41c4eb6425183702c4c82b2e0
ibarta1977 <ibarta1977@gmail.com>
2026-04-19 14:41:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gestionale-lavori-v5.5'; // <--- CAMBIATO A V5.5
+const CACHE_NAME = 'gestionale-lavori-v5.6'; // <--- CAMBIATO A V5.6
 const assets = [
   './',
   './index.html',
